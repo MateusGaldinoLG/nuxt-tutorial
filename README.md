577ec6200033b869d6e365adb480d8b7c3a68496
MateusGaldinoLG <mateuslguilherme@gmail.com>
2021-06-14 11:22:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# nuxt-tutorial
+
+## Tutorial/crash course de Nuxt.js
+
+Esse repositório foi tirado do tutorial de Nuxt.js do canal Laith Harb  
+Link: <https://www.youtube.com/watch?v=Wdmi4k7sFzU&t=4882s>  
+
+Conteudo: routing com Vue.js, nuxt.js, componentes e diretivas, bootstrap e SSR
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <Hero />
+      <Large-card-display
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cards-section="cardInfo"
+      />
+      <Small-card-display
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cards-section="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
+export default {
+  data () {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections
+    }
+  }
+}
+</script>
+
+<style>
+</style>
